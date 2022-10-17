e6d6b7327871ff5cfbeb1af82c3f8593c20783f1
Hazem Gader <hgader@euro.adroot>
2022-10-17 18:08:05 +0200

    start

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,css}"],
+  theme: {
+    extend: {
+
+      colors: {
+        grey_col:{
+          100:'#D5E1EF',
+          200:'#7D889E',
+        }
+      }
+  },
+  plugins: [],
+}
+}
